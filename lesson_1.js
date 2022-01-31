fbe89ca15dec98ea0361cf69b45a5ccad6d79f08
DariaIgo <dkuzavkova@bk.ru>
2022-01-31 22:52:32 +0300

    hw_1

---
+++ lesson_1.js
@@ -0,0 +1,13 @@
+// 1
+let tc = +prompt('Введите температуру в цельсиях:');
+tf = ( 9 / 5) * tc + 32;
+alert(tf)
+
+// 2
+let admin;
+let name = "Василий";
+admin = name;
+alert(admin)
+
+ // 3
+alert(1000 + "108") //1000108
